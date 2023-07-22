e8109a88c4629b3c0fc1fb3d127e3edb5e6115c6
Den <programm.87@mail.ru>
2023-07-22 18:38:53 +0600

    Примитивная запись пользователя в базу данных

--- registration/do_register.php
+++ registration/do_register.php
@@ -2,4 +2,14 @@
 
 require './funcs.php';
 
-dAd($_POST);
+//dAd($_POST);
+
+$username = $_POST["username"];
+$password = $_POST["password"];
+
+$db_user = 'myuser';
+$db_password = '12345';
+
+$db_connect = new PDO("mysql:host=localhost;dbname=examples;charset=utf8;", $db_user, $db_password);
+$sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+$db_connect->exec($sql);
