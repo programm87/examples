82482bc7a4d5e711fe1364df4c70871cef290c22
Den <programm.87@mail.ru>
2023-07-23 12:30:10 +0600

    Добавление пользователя подготовленным запросом

--- registration/do_register.php
+++ registration/do_register.php
@@ -5,16 +5,33 @@ echo realpath('.') . PHP_EOL . '<br>';
 echo dirname(__DIR__);
 //dAd($_POST);
 
-$username = $_POST["username"];
-$password = $_POST["password"];
-$email = $_POST["email"];
-$fhone = $_POST["fhone"];
+$param_user = array();
+
+if (isset($_POST["username"])) {
+    $username = $_POST["username"];
+}
+if (isset($_POST["password"])) {
+    $password = $_POST["password"];
+}
+if (isset($_POST["email"])) {
+    $email = $_POST["email"];
+}
+if (isset($_POST["fhone"])) {
+    $fhone = $_POST["fhone"];
+}
+
 
 $db_user = 'myuser';
 $db_password = '12345';
 
 $db_connect = new PDO("mysql:host=localhost;dbname=examples;charset=utf8;", $db_user, $db_password);
-$sql = "INSERT INTO users (username, password, email, fhone) VALUES ('$username', '$password', '$email', '$fhone')";
-$db_connect->exec($sql);
+$sql = "INSERT INTO users (username, password, email, fhone) VALUES (:username, :password, :email, :fhone)";
+$stmt = $db_connect->prepare($sql); // подготовка запроса, еще без данных
+$stmt->bindValue(':username', $username);
+$stmt->bindValue(':password', $password);
+$stmt->bindValue(':email', $email);
+$stmt->bindValue(':fhone', $fhone);
+
+$stmt->execute();
 
 header( "Location: /registration" );
